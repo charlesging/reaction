8a2f5364e0c5a34afe0779deea3ddc46ea247ed9
Charles Ging <charlesging@Charless-iMac.lan1>
2019-10-30 10:36:44 -0500

    seed data

--- app/javascript/components/Application.jsx
+++ app/javascript/components/Application.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import PropTypes from "prop-types";
 
-import TopNav from './shared/TopNav';
-import BoardsDashboardContainer from './dashboard/BoardsDashboardContainer';
+import TopNav from "./shared/TopNav";
+import BoardsDashboardContainer from "./dashboard/BoardsDashboardContainer";
 
-import { fetchBoards } from '../actions/BoardActions';
+import { fetchBoards } from "../actions/BoardActions";
 
 class Application extends React.Component {
   static contextTypes = {
     store: PropTypes.object
-  }
+  };
 
   componentDidMount() {
     const store = this.context.store;
@@ -27,7 +27,7 @@ class Application extends React.Component {
     return (
       <div>
         <TopNav />
-        <Route path='/' exact component={BoardsDashboardContainer} />
+        <Route path="/" exact component={BoardsDashboardContainer} />
       </div>
     );
   }
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Board.destroy_all
+List.destroy_all
+Card.destroy_all
+
+board = Board.create title: "First Board"
+
+list = List.create title: "first_list", board_id: board.id
+list2 = List.create title: "second_list", board_id: board.id
+
+card1 = Card.create title: "first card", list: list, description: "some desc", labels: ["red", "yellow"]
+card2 = Card.create title: "second card", list: list, description: "desc #2", labels: ["red", "purple"] 
+
+card3 = Card.create title: "third card", list: list2, description: "third card???", labels: ["red", "purple"]
+card4 = Card.create title: "fourth card", list: list2, description: "this is the 4th card", labels: ["red", "purple"]
+card5 = Card.create title: "fifth card", list: list2, description: "FIFTH CaRd", labels: ["red", "purple"] 
