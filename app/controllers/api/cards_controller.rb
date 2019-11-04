d1ba3c9a0c403500ef23bdf4e746bae84f1a5453
William Mills <millswilliamrobert@gmail.com>
2019-11-04 13:23:01 -0500

    added backend route to handle PUT card

--- app/controllers/api/cards_controller.rb
+++ app/controllers/api/cards_controller.rb
@@ -16,9 +16,26 @@ class Api::CardsController < ApplicationController
     @error = "Invalid list id provided."
     render 'api/shared/error', status: 404
   end
+
+  def update
+    @card = Card.find(params[:id])
+    
+    if @card.update(card_params)
+      render :update, status: 200
+    else
+      @error = card.errors.full_messages.join(', ')
+      render 'api/shared/error', status: 422
+    end
+
+  rescue ActiveRecord::RecordNotFound
+    @error = "Invalid card id provided."
+    render 'api/shared/error', status: 404
+
+  end
+
   private 
 
   def card_params
-    params.require(:card).permit(:title)
+    params.require(:card).permit(:title, :list_id, :position, :description, :archived, :due_date, :completed, :labels)
   end
 end
